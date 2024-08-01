f529d4ccad7219265d18aab15a37fa1a57e62c15
radiologiaddu <app.radiologia@ddu.mx>
2024-08-01 16:52:05 -0600

    reporte coordinacion

--- app/Exports/StudyExport.php
+++ app/Exports/StudyExport.php
@@ -42,7 +42,7 @@ class StudyExport implements FromCollection, WithHeadings
             ->where('status', 'Enviado')
             ->orderBy('created_at', 'DESC')
             ->orderBy('date', 'DESC')
-            ->take(800)
+            ->take(600)
             ->get();
 
         foreach ($studies as $study){
