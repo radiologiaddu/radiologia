23299195b6696878494c47a2eafa16cc6e13a00b
radiologiaddu <app.radiologia@ddu.mx>
2024-11-06 14:43:57 -0600

    Ajuste de tamaño para reporte de coordinacion 100

--- app/Exports/StudyExport.php
+++ app/Exports/StudyExport.php
@@ -42,7 +42,7 @@ class StudyExport implements FromCollection, WithHeadings
             ->where('status', 'Enviado')
             ->orderBy('created_at', 'DESC')
             ->orderBy('date', 'DESC')
-            ->take(200)
+            ->take(100)
             ->get();
 
         foreach ($studies as $study){
