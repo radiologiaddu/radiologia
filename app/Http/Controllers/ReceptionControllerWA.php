69f18f46469c54b777bd753c1ed4990035145c4a
radiologiaddu <app.radiologia@ddu.mx>
2025-08-21 14:33:29 -0600

    Delete temp files

--- app/Http/Controllers/ReceptionControllerWA.php
+++ app/Http/Controllers/ReceptionControllerWA.php
@@ -291,6 +291,7 @@ $publicUrls[] = $filename;
                     ]),
                 ]);
                 Log::info("Enviado archivo por WhatsApp: {$url}");
+                Storage::disk('public_pdfs')->delete($url);
             }}
 
             Log::info("WhatsApp enviado correctamente a {$to}");
